dda12ff9a6ea00dddf39c2ad139a6893453a2f21
Misha Kaletsky <mmkal@users.noreply.github.com>
2023-10-24 09:12:09 -0400

    id=(3,4) -> data-xy=3,4

--- src/cell-map.mjs
+++ src/cell-map.mjs
@@ -2,17 +2,26 @@ import { assert } from './helpers.mjs';
 
 //  Internally used map of Crossword model data to DOM elements.
 class CellMap {
+  /** @type {Record<string, any>} */
   #modelCells = {};
 
-  //  Adds a Cell <-> Cell Element mapping.
+  /**
+   * Adds a Cell <-> Cell Element mapping.
+   * @param {*} modelCell 
+   * @param {HTMLDivElement} cellElement 
+   */
   add(modelCell, cellElement) {
     assert(modelCell, 'modelCell is null or undefined');
     assert(cellElement, 'cellElement is null or undefined');
-    // cellElement.id set in CrosswordController.#newCellElement()
-    this.#modelCells[cellElement.id] = modelCell;
+    // cellElement.dataset.xy set in CrosswordController.#newCellElement()
+    this.#modelCells[cellElement.dataset.xy] = modelCell;
   }
 
-  //  Gets the DOM element for a modelCell.
+  /**
+   * Gets the DOM element for a modelCell.
+   * @param {*} modelCell 
+   * @returns {HTMLDivElement}
+   */
   cellElement = (modelCell) => {
     assert(typeof modelCell === 'object', 'Cell is not an object');
     // modelCell.cellElement set in CrosswordController.#newCellElement()
@@ -25,7 +34,7 @@ class CellMap {
       case 'string':
         return this.#modelCells[cellElement];
       case 'object':
-        return this.#modelCells[cellElement.id];
+        return this.#modelCells[cellElement.dataset.xy];
       default:
         assert(true, 'Unexpected type for "cellElement"');
         break;
